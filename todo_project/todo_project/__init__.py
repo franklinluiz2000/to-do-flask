4cb966f90f1663beb475cdf47ac19294630ae5c3
franklinluiz2000 <franklinluiz2000@gmail.com>
2024-09-12 00:03:05 -0300

    Modificando os testes

--- todo_project/todo_project/__init__.py
+++ todo_project/todo_project/__init__.py
@@ -1,21 +1,19 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+from flask_bcrypt import Bcrypt
 
-db = SQLAlchemy()
-bcrypt = Bcrypt()
-login_manager = LoginManager()
 
-def create_app():
-    app = Flask(__name__)
-    app.config.from_object('config_module')  # Carregue as configurações corretas
+app = Flask(__name__)
+app.config['SECRET_KEY'] = '45cf93c4d41348cd9980674ade9a7356'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
 
-    db.init_app(app)
-    bcrypt.init_app(app)
-    login_manager.init_app(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login' 
+login_manager.login_message_category = 'danger'
 
-    with app.app_context():
-        from todo_project import routes  # Importe as rotas dentro do contexto
+bcrypt = Bcrypt(app)
 
-    return app
+# Always put Routes at end
+from todo_project import routes
