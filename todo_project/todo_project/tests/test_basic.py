891bf632908b5461667ba3f9746c938905216408
franklinluiz2000 <franklinluiz2000@gmail.com>
2024-09-11 23:50:45 -0300

    Modificando os testes

--- todo_project/todo_project/tests/test_basic.py
+++ todo_project/todo_project/tests/test_basic.py
@@ -22,89 +22,89 @@ def test_client():
             db.drop_all()  # Remove o banco de dados após os testes
 
 
-# @pytest.fixture(scope='module')
-# def init_database():
-#     db.create_all()
-#     yield db
-#     db.drop_all()
+@pytest.fixture(scope='module')
+def init_database():
+    db.create_all()
+    yield db
+    db.drop_all()
 
-# def test_user_model(init_database):
-#     user = User(username='testuser', password=generate_password_hash('password'))
-#     db.session.add(user)
-#     db.session.commit()
-#     assert user.username == 'testuser'
-#     assert check_password_hash(user.password, 'password')
+def test_user_model(init_database):
+    user = User(username='testuser', password=generate_password_hash('password'))
+    db.session.add(user)
+    db.session.commit()
+    assert user.username == 'testuser'
+    assert check_password_hash(user.password, 'password')
 
-# def test_register(test_client):
-#     response = test_client.post('/register', data=dict(
-#         username='newuser',
-#         password='newpassword',
-#         confirm_password='newpassword'
-#     ), follow_redirects=True)
-#     assert b'Account Created For' in response.data
+def test_register(test_client):
+    response = test_client.post('/register', data=dict(
+        username='newuser',
+        password='newpassword',
+        confirm_password='newpassword'
+    ), follow_redirects=True)
+    assert b'Account Created For' in response.data
 
-# def test_login(test_client, init_database):
-#     user = User(username='testuser', password=generate_password_hash('password'))
-#     db.session.add(user)
-#     db.session.commit()
-#     response = test_client.post('/login', data=dict(
-#         username='testuser',
-#         password='password'
-#     ), follow_redirects=True)
-#     assert b'Login Successful' in response.data
+def test_login(test_client, init_database):
+    user = User(username='testuser', password=generate_password_hash('password'))
+    db.session.add(user)
+    db.session.commit()
+    response = test_client.post('/login', data=dict(
+        username='testuser',
+        password='password'
+    ), follow_redirects=True)
+    assert b'Login Successful' in response.data
 
-# def test_add_task(test_client, init_database):
-#     user = User(username='testuser', password=generate_password_hash('password'))
-#     db.session.add(user)
-#     db.session.commit()
-#     test_client.post('/login', data=dict(
-#         username='testuser',
-#         password='password'
-#     ), follow_redirects=True)
-#     response = test_client.post('/add_task', data=dict(
-#         task_name='New Task'
-#     ), follow_redirects=True)
-#     assert b'Task Created' in response.data
+def test_add_task(test_client, init_database):
+    user = User(username='testuser', password=generate_password_hash('password'))
+    db.session.add(user)
+    db.session.commit()
+    test_client.post('/login', data=dict(
+        username='testuser',
+        password='password'
+    ), follow_redirects=True)
+    response = test_client.post('/add_task', data=dict(
+        task_name='New Task'
+    ), follow_redirects=True)
+    assert b'Task Created' in response.data
 
-# def test_update_task(test_client, init_database):
-#     user = User(username='testuser', password=generate_password_hash('password'))
-#     db.session.add(user)
-#     db.session.commit()
-#     test_client.post('/login', data=dict(
-#         username='testuser',
-#         password='password'
-#     ), follow_redirects=True)
-#     test_client.post('/add_task', data=dict(
-#         task_name='Old Task'
-#     ), follow_redirects=True)
-#     task = Task.query.first()
-#     response = test_client.post(f'/all_tasks/{task.id}/update_task', data=dict(
-#         task_name='Updated Task'
-#     ), follow_redirects=True)
-#     assert b'Task Updated' in response.data
+def test_update_task(test_client, init_database):
+    user = User(username='testuser', password=generate_password_hash('password'))
+    db.session.add(user)
+    db.session.commit()
+    test_client.post('/login', data=dict(
+        username='testuser',
+        password='password'
+    ), follow_redirects=True)
+    test_client.post('/add_task', data=dict(
+        task_name='Old Task'
+    ), follow_redirects=True)
+    task = Task.query.first()
+    response = test_client.post(f'/all_tasks/{task.id}/update_task', data=dict(
+        task_name='Updated Task'
+    ), follow_redirects=True)
+    assert b'Task Updated' in response.data
 
-# def test_delete_task(test_client, init_database):
-#     user = User(username='testuser', password=generate_password_hash('password'))
-#     db.session.add(user)
-#     db.session.commit()
-#     test_client.post('/login', data=dict(
-#         username='testuser',
-#         password='password'
-#     ), follow_redirects=True)
-#     test_client.post('/add_task', data=dict(
-#         task_name='Task to be deleted'
-#     ), follow_redirects=True)
-#     task = Task.query.first()
-#     response = test_client.get(f'/all_tasks/{task.id}/delete_task', follow_redirects=True)
-#     assert b'Task Deleted' in response.data
+def test_delete_task(test_client, init_database):
+    user = User(username='testuser', password=generate_password_hash('password'))
+    db.session.add(user)
+    db.session.commit()
+    test_client.post('/login', data=dict(
+        username='testuser',
+        password='password'
+    ), follow_redirects=True)
+    test_client.post('/add_task', data=dict(
+        task_name='Task to be deleted'
+    ), follow_redirects=True)
+    task = Task.query.first()
+    response = test_client.get(f'/all_tasks/{task.id}/delete_task', follow_redirects=True)
+    assert b'Task Deleted' in response.data
 
-# def test_account_page(test_client, init_database):
-#     user = User(username='testuser', password=generate_password_hash('password'))
-#     db.session.add(user)
-#     db.session.commit()
-#     test_client.post('/login', data=dict(
-#         username='testuser',
-#         password='password'
-#     ), follow_redirects=True)
-#     response = test_client.get('/account')
-#     assert response.status_code == 200
+def test_account_page(test_client, init_database):
+    user = User(username='testuser', password=generate_password_hash('password'))
+    db.session.add(user)
+    db.session.commit()
+    test_client.post('/login', data=dict(
+        username='testuser',
+        password='password'
+    ), follow_redirects=True)
+    response = test_client.get('/account')
+    assert response.status_code == 200
