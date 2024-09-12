307492771fead40273ebeeef4192badc524a3d7c
franklinluiz2000 <franklinluiz2000@gmail.com>
2024-09-11 23:38:06 -0300

    Modificando os testes

--- todo_project/todo_project/tests/test_basic.py
+++ todo_project/todo_project/tests/test_basic.py
@@ -1,3 +1,7 @@
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../..')))
+
 import pytest
 from flask import Flask, url_for
 from flask_sqlalchemy import SQLAlchemy
