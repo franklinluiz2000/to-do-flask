b657a7a58f7d62caa34e554a39ae06732950b84d
franklinluiz2000 <franklinluiz2000@gmail.com>
2024-09-22 07:35:27 -0300

    Testes com o Bandit

--- run.py
+++ run.py
@@ -3,4 +3,4 @@ from todo_project import app
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))  # Use a porta do Heroku ou 5000 localmente
-    app.run(host='0.0.0.0', port=port, debug=True)
+    app.run(host='127.0.0.1', port=port, debug=True)
